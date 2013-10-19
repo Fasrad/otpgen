b210de344221821bb3736e007a7c664465dc12b3
Fasrad <millerchaz@gmail.com>
2013-10-18 20:58:30 -0400

    dencipher has been known to work

--- dencipher.pl
+++ dencipher.pl
@@ -2,20 +2,20 @@
 
 %alpha = ( A => 0, B => 1, C => 2, D => 3, E => 4, F => 5, G => 6, H => 7, I => 8, J => 9, K => 10, L => 11, M => 12, N => 13, O => 14, P => 15, Q => 16, R => 17, S => 18, T => 19, U => 20, V => 21, W => 22, X => 23, Y => 24, Z => 25); 
 
-%dig = ( 0 => A, 1 => B, 2 => C, 3 => D, 4 => E, 5 => F, 6 => G, 7 => H, 8 => I, 9 => J, 10 => K, 11 => L, 12 => M, 13 => N, 14 => O, 15 => P, 16 => Q, 17 => R, 18 => S, 19 => T, 20 => U, 21 => V, 22 => W, 23 => X, 24 => Y, 25 => Z
- ); 
+%dig = ( 0 => A, 1 => B, 2 => C, 3 => D, 4 => E, 5 => F, 6 => G, 7 => H, 8 => I, 9 => J, 10 => K, 11 => L, 12 => M, 13 => N, 14 => O, 15 => P, 16 => Q, 17 => R, 18 => S, 19 => T, 20 => U, 21 => V, 22 => W, 23 => X, 24 => Y, 25 => Z); 
 
 LOOP:
 while(){
-    print "enter E to encrypt a message, D to decrypt a message:\n";
+    print "enter E to encrypt a message, D to decrypt a message, Q to quit:\n";
     $choice=uc(<STDIN>);
     chomp $choice;
     if ($choice eq "E"){
         &encrypt;
     } elsif ($choice eq "D") {
         &decrypt;
+    } elsif ($choice eq "Q") {
+        exit;
     } else {
-        print "enter E to encrypt a message, D to decrypt a message:\n";
         next LOOP;
     }
 }
@@ -26,25 +26,25 @@ sub encrypt{
     chomp $mess;
     $mess=uc($mess);
     $mess=~s/\s//g;
+    if($mess=~/[^A-Z]/){print "Illegal characters in message.\n"; return}
 
     print "enter your pad text:";
     $pad=<STDIN>;
     chomp $pad;
     $pad=uc($pad);
     $pad=~s/\s//g;
+    if($mess=~/[^A-Z]/){print "Illegal characters in pad text.\n"; return}
 
     print "your ciphertext is as follows:\n";
-    @pad=split'',$pad;
-    @message=split'',$mess;
+    @pad=split'',$pad; $pad_length=scalar(@pad);
+    @message=split'',$mess; $mess_length=scalar(@message);
+    if($mess_length > $pad_length){print "Not enough pad!\n"; return}
+
     $ind=0;
-    while(@message){
-        print "ind:$ind,";
+    while($ind < $mess_length){
         $messd=$alpha{$message[$ind]};
-        print "messd:$messd,";
         $padd=$alpha{$pad[$ind]};
-        print "padd:$padd,";
-        $cypher=($padd+$messd) % 25;
-        print "cypher:$cypher,";
+        $cypher=($padd+$messd) % 26;
         print $dig{$cypher};
         $ind++;
     }
@@ -58,20 +58,27 @@ sub decrypt{
     chomp $mess;
     $mess=uc($mess);
     $mess=~s/\s//g;
+    if($mess=~/[^A-Z]/){print "Illegal characters in ciphertext. Cannot continue.\n"; return}
 
     print "enter your pad text:";
     $pad=<STDIN>;
     chomp $pad;
     $pad=uc($pad);
     $pad=~s/\s//g;
+    if($mess=~/[^A-Z]/){print "Illegal characters in pad text. Cannot continue.\n"; return}
 
-    print "your plaintext is as follows:\n";
-    foreach(split '', $mess){
-        $messd=$alpha{$mess};
-        $padd=$alpha{$pad};
-        $cypher=($messd-$padd) % 25;
-        print $cypher;
+    print "your decrypted message is as follows:\n";
+    @pad=split'',$pad; $pad_length=scalar(@pad);
+    @message=split'',$mess; $mess_length=scalar(@message);
+    if($mess_length > $pad_length){print "Not enough pad!\n"; return}
+
+    $ind=0;
+    while($ind < $mess_length){
+        $messd=$alpha{$message[$ind]};
+        $padd=$alpha{$pad[$ind]};
+        $cypher=($messd-$padd) % 26;
         print $dig{$cypher};
+        $ind++;
     }
     print "\n";
     return;
